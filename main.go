f8a50ed1f00a8a59179bbb1905c29994efe5a2dd
lier <lier0205@163.com>
2025-06-05 21:41:59 +0800

    25-6-5

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello gin")
 	r := gin.Default()
+
+	r.GET("/hello_gin", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	shopGroup := r.Group("/shop", func1, func2)
+	shopGroup.Use(func3)
+	{
+		shopGroup.GET("/index", func4, func5)
+	}
+
+	r.Run(":9999")
 }
